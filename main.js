eda656c3a3e6b30a97c799fb5b532b2b95025cf8
Apurba Debnath <apurba871@gmail.com>
2020-09-25 11:32:36 +0530

    Added codes for player 1

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.getElementById("p2");
+
+var p1Score = 0;
+
+var player1_score = document.querySelector("#p1_score");
+var target = document.querySelector("#target_score");
 
 p1Button.addEventListener("click", function() {
-    alert("Clicked");
-})
+    if (p1Score < target.textContent)
+        p1Score++;
+    player1_score.textContent = p1Score;
+});
